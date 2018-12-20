245d45c2500e930279ba40bf99ca4af4f540415f
RyuSeungHwan2 <shr2833@gamil.com>
2018-12-20 14:17:51 +0900

    꿈해몽(검색어) return dic{페이지번호 : 'href'}

---
+++ test2/search_dream.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+print('연습용 실행')
+# 크롬 드라이버 설정
+
+def searchDream(text):
+    driver = webdriver.Chrome(r'C:\Users\student\Desktop\chromedriver_win32\chromedriver.exe')
+    # 웹사이트 열기
+    driver.get('https://fortune13.nate.com/v1/dream/dream/dream.asp?1=1')
+    # 검색창 찾기
+    search_text = driver.find_element_by_css_selector('input#memo.inT')
+    # 검색바에 우유 적기
+    search_text.send_keys(text)
+
+    btn = driver.find_element_by_id('btnSearch')
+    btn.click()
+
+    source = driver.page_source
+    soup = BeautifulSoup(source, 'html.parser')
+
+    titles = []
+    title_nums = []
+    href_list = []
+    pre_url = 'https://fortune13.nate.com/v1/dream/dream/'
+    for i in soup.find_all('td', class_="tit"):
+        href_list.append(pre_url + i.find('a')['href'])
+        titles.append(i.get_text().strip())
+    for n in soup.find_all('td', class_="num"):
+        title_nums.append(int(n.get_text().strip()))
+
+    num_to_title = dict(zip(title_nums, titles))
+    num_to_href = dict(zip(title_nums, href_list))
+    # print('titles :', titles)
+    # print('nums :', title_nums)
+    # print('href :', href_list)
+    # print(num_to_title)
+    # print(num_to_href)
+    return num_to_href
+# searchDream('바다')
+
+# 꿈 - 입력대기 - 서치드림(입력값) -
+
+#num
+
